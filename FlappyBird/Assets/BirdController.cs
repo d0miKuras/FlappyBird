6a832c8d436715c53c7b2f3b2f03b40737133e96
d0miKuras <dominik.kurasbediani@gmail.com>
2022-01-15 18:43:35 +0100

    Added bird animation, jump controls, ground and bird colliders, setup the editor

---
+++ FlappyBird/Assets/BirdController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdController : MonoBehaviour
+{
+    [SerializeField] private float velocity = 1.0f;
+    private Rigidbody2D rb;
+    private Touch touch;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began)
+            {
+                rb.velocity = Vector2.up * velocity;
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log("Collided");
+    }
+}
