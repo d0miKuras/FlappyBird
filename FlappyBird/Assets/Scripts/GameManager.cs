05f4690c866838cd76f2720155069bf73cb09583
d0miKuras <dominik.kurasbediani@gmail.com>
2022-01-19 18:49:27 +0100

    Added score text & new high score texts to game over screen

--- FlappyBird/Assets/Scripts/GameManager.cs
+++ FlappyBird/Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public enum GameState
 {
@@ -14,7 +15,6 @@ public enum GameState
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private BirdController birdController;
-    [SerializeField] private GameObject gameOverWindow;
 
     private PipeSpawner pipeSpawner;
     private ScoreManager scoreManager;
@@ -32,8 +32,7 @@ public class GameManager : MonoBehaviour
         ui = GetComponent<UIManager>();
         GameState = GameState.GameStart;
         gameOverWindowShown = false;
-        if (gameOverWindow)
-            gameOverWindow.SetActive(false);
+        ui.ShowGameOverWindow(false);
     }
 
     void Update()
@@ -49,19 +48,20 @@ public class GameManager : MonoBehaviour
             {
                 GameState = GameState.GameOver; // Set the game state to prevent pipes from moving
                 birdController.SetGravity(false); // Stop bird from moving
-                if (gameOverWindow && !gameOverWindowShown) // Show Game Over screen
+                if (!gameOverWindowShown) // Show Game Over screen
                 {
-                    gameOverWindow.SetActive(true);
-                    gameOverWindowShown = true;
-                    // TODO: Show score in Game Over window
+                    ui.ShowGameOverWindow(true);
+                    gameOverWindowShown = true; // To prevent this branch from running multiple times
+                    ui.SetScoreText($"SCORE: {scoreManager.Score}");
                     if (highScoreManager.IsTopFive(scoreManager.Score)) // If the score is top 5 of high scores, 
                                                                         // let the user know and add it to the list
                     {
                         highScoreManager.AddHighScore(scoreManager.Score);
-                        Debug.Log("Score added to the top 5 high scores!"); // TODO: show on screen
+                        ui.ShowHighScoreText(true);
                     }
                 }
             }
+            // TODO: Pause
             else
             {
                 if (Input.touchCount > 1 && scoreManager.BombCount > 0)
@@ -78,10 +78,4 @@ public class GameManager : MonoBehaviour
         pipeSpawner.DestroyObstaclesOnScreen();
         scoreManager.DecrementBomb();
     }
-
-    public void RestartLevel()
-    {
-        SceneManager.LoadScene("GameScene");
-    }
-
 }
--- FlappyBird/Assets/Scripts/UIManager.cs
+++ FlappyBird/Assets/Scripts/UIManager.cs
@@ -2,19 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    [SerializeField] private GameObject gameOverWindow;
 
+    void Awake()
+    {
+        if (gameOverWindow)
+        {
+            ShowHighScoreText(false);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
 
+    }
+
+    public void ShowGameOverWindow(bool value)
+    {
+        if (gameOverWindow)
+            gameOverWindow.SetActive(value);
     }
 
     public void LoadMainMenu()
@@ -25,5 +36,21 @@ public class UIManager : MonoBehaviour
     public void LoadGame()
     {
         SceneManager.LoadScene("GameScene");
+    }
+
+    public void SetScoreText(string _text)
+    {
+        if (gameOverWindow)
+        {
+            gameOverWindow.transform.Find("ScoreText").GetComponent<Text>().text = _text;
+        }
+    }
+
+    public void ShowHighScoreText(bool value)
+    {
+        if (gameOverWindow)
+        {
+            gameOverWindow.transform.Find("NewHighScore").gameObject.SetActive(value);
+        }
     }
 }
